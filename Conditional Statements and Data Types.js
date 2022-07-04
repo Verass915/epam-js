78f908da6afdffac5d371099ddb2f5a8aaeb7fca
Verass <veraaksucic@gmail.com>
2022-07-04 11:41:17 +0400

    modify

--- Conditional Statements and Data Types.js
+++ Conditional Statements and Data Types.js
@@ -1,65 +1,60 @@
 //+
-let a = false;
-let b = "string";
-let ab = a + b;
-console.log (ab); //falsestring
+let boolean1 = false;
+let string1 = "string";
+let addingBooleanToString = boolean1 + string1;
+console.log (addingBooleanToString); //falsestring
 
-let c = "string";
-let d = 15;
-let cd = c + d;
-console.log (cd); //string15
+let string2 = "string";
+let number2 = 15;
+let addingStringToNumber = string2 + number2;
+console.log (addingStringToNumber); //string15
 
-let e = 15;
-let f = false;
-let ef = e + f;
-console.log (ef); //15
+let number3 = 15;
+let boolean3 = false;
+let addingNumberToBoolean = number3 + boolean3;
+console.log (addingNumberToBoolean); //15
 
 //*
-let g = "string";
-let h = false;
-let gh = g * h;
-console.log (gh); // NaN
+let string4 = "string";
+let boolean4 = false;
+let multipleStringByBoolean = string4 * boolean4;
+console.log (multipleStringByBoolean); // NaN
 
-let i = "string";
-let j = 15;
-let ij = i * j;
-console.log (ij); // NaN
+let string5 = "string";
+let number5 = 15;
+let multipleStringByNumber = string5 * number5;
+console.log (multipleStringByNumber); // NaN
 
-let k = 15;
-let l = false;
-let kl = k * l;
-console.log (kl); // 0
+let number6 = 15;
+let boolean6 = false;
+let multipleNumberByBoolean = number6 * boolean6;
+console.log (multipleNumberByBoolean); // 0
 
 // /
-let m = "string";
-let n = false;
-let mn = m / n;
-console.log (mn); // NaN 
+let string7 = "string";
+let boolean7 = false;
+let devideStringByBoolean = string7 / boolean7;
+console.log (devideStringByBoolean); // NaN 
 
-let o = "string";
-let p = 15;
-let op = o / p;
-console.log (op); // NaN
+let string8 = "string";
+let number8 = 15;
+let devideStringByNumber = string8 / number8;
+console.log (devideStringByNumber); // NaN
 
-let q = 15;
-let r = false;
-let qr = q / r;
-console.log (qr); // Infinity
+let number9 = 15;
+let boolean9 = false;
+let devideNumberToBoolean = number9 / boolean9;
+console.log (devideNumberToBoolean); // Infinity
 
 // conversion
-let s = "546";
+let s = "";
 console.log(typeof s); // string
 
-let t = Number(s); // becomes a number 546
-console.log(typeof t); // number
+s = Number(s); // becomes a number
+console.log(typeof s); // number
 
-
-
-let u = 546;
-console.log(typeof u); // number
-
-u = String(u); 
-console.log(typeof u); // string
+s = String(s); 
+console.log(typeof s); // string
 
 
 
